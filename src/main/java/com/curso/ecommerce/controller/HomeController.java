36305ad3f226acffe003c23b28446e3511832ca2
LeonardoVegaAnanda <lvega@anandaproducts.com>
2022-12-29 13:45:21 -0600

    Mostrar la vista del producto al usuario

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -1,16 +1,19 @@
 package com.curso.ecommerce.controller;
 
 import com.curso.ecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
     @Autowired
     private ProductoService sProducto;
 
@@ -18,6 +21,11 @@ public class HomeController {
     public String home(Model model){
         model.addAttribute("productos",sProducto.finAll());
         return "usuario/home";
+    }
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable Integer id){
+        log.info("Id Producto enviado como Parametro {}",id);
+        return "usuario/productohome";
     }
 
 }
