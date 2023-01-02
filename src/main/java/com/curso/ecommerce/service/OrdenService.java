79aa0e5392223a0cbfaf73181049615f026f719a
LeonardoVegaAnanda <lvega@anandaproducts.com>
2023-01-02 11:03:07 -0600

    Metodo para generar el numero de la orden

--- src/main/java/com/curso/ecommerce/service/OrdenService.java
+++ src/main/java/com/curso/ecommerce/service/OrdenService.java
@@ -2,7 +2,11 @@ package com.curso.ecommerce.service;
 
 import com.curso.ecommerce.model.Orden;
 
+import java.util.List;
+
 public interface OrdenService {
 
     Orden save(Orden orden);
+
+    List<Orden> findAll();
 }
