f9eb371e01bf0c5a4ab72db4f94438a023102446
LeonardoVegaAnanda <lvega@anandaproducts.com>
2023-01-10 13:00:47 -0600

    Mostrar vista compras para user

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,5 +57,11 @@ public class UsuarioController {
             logger.info("USUARIO NO EXITENTE");
         }
     return "redirect:/";
+    }
+
+    @GetMapping("compras")
+    public String obtenerCompras(Model model, HttpSession session){
+        model.addAttribute("sesion",session.getAttribute("idUsuario"));
+        return "usuario/compras";
     }
 }
